d58ab43b98f4299ac6415f02f068fab5d6ed67be
Ghostwolf73 <solomzigumbo@gmail.com>
2024-09-30 16:55:49 +0200

    added loopback interfaces configuration

--- Base configuration.py
+++ Base configuration.py
@@ -13,7 +13,16 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
-tn.write(b"ls\n")
-tn.write(b"exit\n")
+tn.write("enable\n")
+tn.write("cisco\n")
+tn.write("conf t\n")
+tn.write("int loop 0\n")
+tn.write("ip address 1.1.1.1 255.255.255.255\n")
+tn.write("int loop 1\n")
+tn.write("ip address 2.2.2.2 255.255.255.255\n")
+tn.write("router ospf 1\n")
+tn.write("network 0.0.0.0 255.255.255.255 area 0\n")
+tn.write("end\n")
+tn.write("exit\n")
 
 print(tn.read_all().decode('ascii'))
