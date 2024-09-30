5c7ea820c651e3237af4e88b2fff5dbcfd5a4f7b
Ghostwolf73 <solomzigumbo@gmail.com>
2024-09-30 17:18:00 +0200

    removed the ospf command because I don't need it yet

--- Base configuration.py
+++ Base configuration.py
@@ -20,8 +20,6 @@ tn.write("int loop 0\n")
 tn.write("ip address 1.1.1.1 255.255.255.255\n")
 tn.write("int loop 1\n")
 tn.write("ip address 2.2.2.2 255.255.255.255\n")
-tn.write("router ospf 1\n")
-tn.write("network 0.0.0.0 255.255.255.255 area 0\n")
 tn.write("end\n")
 tn.write("exit\n")
 
